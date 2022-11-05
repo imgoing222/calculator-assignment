bdd813d4eaff6fc451fb3eefeb698135812a8268
9603100 <imgoing222@gmail.com>
2022-11-05 20:46:12 +0900

    feat: 간단한 더하기 테스트 작성

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,5 +1,10 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("계산기 작동 테스트", () => {
+	it("2 + 3 = 5", () => {
+		cy.visit("http://127.0.0.1:5500/index.html");
+		cy.contains("button", "2").click();
+		cy.contains(".sum", "+").click();
+		cy.contains("button", "3").click();
+		cy.contains("button", "=").click();
+		cy.get(".result").should("have.value", "5");
+	});
+});
