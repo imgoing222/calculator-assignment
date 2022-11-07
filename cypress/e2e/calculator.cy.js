0d85e4aacf30a94e7ae30ea1ad3b086f365f697f
9603100 <imgoing222@gmail.com>
2022-11-08 02:59:57 +0900

    test: 연산 테스트코드 작성

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,10 +1,21 @@
-describe("계산기 작동 테스트", () => {
-	it("2 + 3 = 5", () => {
+const clickDigit = (text) => cy.contains(".digit", text).click();
+const clickOperator = (text) => cy.contains(".operator", text).click();
+const getDisplayNumber = (text) => cy.get(".result").should("have.text", text);
+
+describe("연산 테스트", () => {
+	it("-1 + 3 + 5 * 2", () => {
 		cy.visit("http://127.0.0.1:5500/index.html");
-		cy.contains("button", "2").click();
-		cy.contains(".sum", "+").click();
-		cy.contains("button", "3").click();
-		cy.contains("button", "=").click();
-		cy.get(".result").should("have.value", "5");
+
+		clickDigit("1");
+		clickOperator("+/-");
+		clickOperator("+");
+		clickDigit("3");
+		clickOperator("+");
+		clickDigit("5");
+		clickOperator("x");
+		clickDigit("2");
+		clickOperator("=");
+
+		getDisplayNumber("12");
 	});
 });
