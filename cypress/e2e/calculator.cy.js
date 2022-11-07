f1b1bc5737b5d85508df25e27ea5776fdd6906bf
9603100 <imgoing222@gmail.com>
2022-11-08 04:59:46 +0900

    test: 그 외 기능 테스트코드 작성

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -19,3 +19,28 @@ describe("연산 테스트", () => {
 		getDisplayNumber("12");
 	});
 });
+
+describe("버튼 테스트", () => {
+	it("C 버튼 동작", () => {
+		cy.visit("http://127.0.0.1:5500/index.html");
+
+		clickDigit("1");
+		clickDigit("3");
+		clickOperator("C");
+
+		getDisplayNumber("0");
+	});
+
+	it("% 버튼 동작", () => {
+		cy.visit("http://127.0.0.1:5500/index.html");
+
+		clickDigit("1");
+		clickDigit("0");
+		clickDigit("0");
+		clickOperator("%");
+		clickOperator("+");
+		clickDigit("7");
+
+		getDisplayNumber("107");
+	});
+});
